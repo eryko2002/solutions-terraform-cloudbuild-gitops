6d28329db654979e9f5ce0bb34f32a3df8c924da
eglab2002 <eglab2002@gmail.com>
2026-03-25 10:48:01 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="refined-kite-451015-n6"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "refined-kite-451015-n6-tfstate"
     prefix = "env/prod"
   }
 }
